49bcd077d645df6b242b3bce0f1dfba15525862a
Kevin Beeler <kbeeler07@gmail.com>
2018-05-14 11:05:47 -0400

    folder route update

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+// Sets up the Express App
+// =============================================================
 var PORT = process.env.PORT || 8080;
-
 var app = express();
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -20,13 +24,21 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/relocation_controller.js");
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/relocation_controller.js");
 
-app.use(routes);
+// app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
