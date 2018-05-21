10e69a2bfc2f35531566d74a7cd905e0a8f160f9
Kristen Kirchgesler <kristen.arbutiski@gmail.com>
2018-05-21 18:25:20 -0400

    update config

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable],config.database, config.username, config.password, {dialect:config.dialect,storage:config.storage});
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
